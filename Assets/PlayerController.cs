06485ad12d15ddeab549f2340567adac4ae32d64
skr47ch_g4m3z <its.me.arrived@gmail.com>
2016-07-21 01:34:46 +0530

    Restart - Infinite Jumper

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(2f, 5f);
+	int direction = 1;
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		float moveDirection = Input.GetAxisRaw("Horizontal");
+		Debug.Log(moveDirection);
+		transform.Translate(Vector2.right * speed.x * moveDirection * Time.deltaTime);
+		transform.Translate(Vector2.up * speed.y * direction * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D collison) {
+		if(collison.gameObject.CompareTag("Walls")) {
+			direction *= -1;
+			Debug.Log("Collisio");
+		}
+	}
+}
