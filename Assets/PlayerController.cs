a19af4022f178f2278d957c105008371e39e25e8
skr47ch_g4m3z <its.me.arrived@gmail.com>
2016-07-22 00:14:58 +0530

    no progress

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,24 +3,54 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public Vector2 speed = new Vector2(2f, 5f);
+	public Transform groundCheck;
+	public LayerMask mask;
+
+	public float speed = 2f;
 	int direction = 1;
-	
+	public float jumpForce;
+	Rigidbody2D body;
+	Vector2 lastFrameVelocity;
+	bool isGrounded = false;
+
 	void Start () {
-	
+		body = GetComponent<Rigidbody2D>();
 	}
 
-	void Update () {
-		float moveDirection = Input.GetAxisRaw("Horizontal");
-		Debug.Log(moveDirection);
-		transform.Translate(Vector2.right * speed.x * moveDirection * Time.deltaTime);
-		transform.Translate(Vector2.up * speed.y * direction * Time.deltaTime);
-	}
+	void FixedUpdate () {
 
-	void OnCollisionEnter2D(Collision2D collison) {
-		if(collison.gameObject.CompareTag("Walls")) {
-			direction *= -1;
-			Debug.Log("Collisio");
+		RaycastHit2D hit = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.01f, mask);
+		Debug.DrawRay(groundCheck.position, Vector2.down * 0.01f, Color.red);
+		if(hit) isGrounded = true;
+
+
+		Debug.Log(isGrounded);
+
+//		isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1<< LayerMask.NameToLayer("Ground"));
+		if(isGrounded) {
+
+			isGrounded = false;
+			body.AddForce(Vector2.up * jumpForce);
 		}
+		float moveDirection = Input.GetAxisRaw("Horizontal");
+//		Debug.Log(moveDirection);
+		transform.Translate(Vector2.right * speed * moveDirection * Time.deltaTime);
+		//transform.Translate(Vector2.up * jumpVelocity * direction * Time.deltaTime);
+//		lastFrameVelocity = body.velocity;
 	}
+
+//	void Jump() {
+//		body.AddForce(Vector2.up * jumpForce);
+//		isGrounded = false;
+//	}
+
+//	void OnCollisionEnter2D(Collision2D collision) {
+//		if(collision.gameObject.CompareTag("Walls")) {
+//			isGrounded = true;	
+//		}
+//	}
+
+//	void OnCollisionExit2D() {
+//		isGrounded = false;
+//	}
 }
